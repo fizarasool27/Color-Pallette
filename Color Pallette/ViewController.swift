315636b7c05d240abd098a6e74108237733e4347
fizarasool27 <fizafr2127@gmail.com>
2020-05-14 04:07:49 +0530

    First Coomit

--- Color Pallette/ViewController.swift
+++ Color Pallette/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        changeColorComponent(self)
     }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changeColorComponent(_ sender: Any) {
+        
+        let r : CGFloat = CGFloat(self.redSlider.value)
+        let g : CGFloat = CGFloat(self.greenSlider.value)
+        let b : CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
     }
-
+    
 
 }
 
